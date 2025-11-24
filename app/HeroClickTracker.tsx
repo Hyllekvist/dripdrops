bd23203ce606364966903f92989856eed4c729e6
Hyllekvist <42335892+Hyllekvist@users.noreply.github.com>
2025-11-24 13:22:56 +0100

    Create HeroClickTracker.tsx

---
+++ app/HeroClickTracker.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { trackEvent } from "../lib/gtag";
+
+type Props = {
+  itemId: string;
+  children: React.ReactNode;
+};
+
+export function HeroClickTracker({ itemId, children }: Props) {
+  return (
+    <div
+      onClick={() =>
+        trackEvent("dd_hero_click", {
+          item_id: itemId,
+          position: "mobile_hero",
+        })
+      }
+    >
+      {children}
+    </div>
+  );
+}
